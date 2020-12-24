4625593a39345ad9135aab9885ddc313474459e7
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-12-23 21:55:31 -0500

    Added Cart component and styling

---
+++ src/components/cart/cart.component.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Container, 
+    Typography, 
+    Button, 
+    Grid} from '@material-ui/core';
+import useStyles from './cart.styles';
+
+const Cart = ({ cart }) => {
+    const isEmpty = !cart.line_items;
+    const classes = useStyles();
+
+    const EmptyCart = () => (
+        <Typography variant='subtitle1'>Your cart is empty, start shopping!</Typography>
+    );
+
+    const FilledCart = () => (
+        <>
+            <Grid container spacing={3}>
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        <div>{item.name}</div>
+                    </Grid>
+                ))}
+            </Grid>
+            <div className={classes.cartDetails}>
+                    <Typography variant='h4'>
+                        Subtotal: {cart.subtotal.formatted_with_symbol}
+                    </Typography>
+                    <div>
+                        <Button className={classes.emptyButton}
+                            size='large'
+                            type='button'
+                            variant='contained'
+                            color='secondary'
+                        >
+                            Empty Cart
+                        </Button>
+                        <Button className={classes.checkoutButton}
+                            size='large'
+                            type='button'
+                            variant='contained'
+                            color='primary'
+                        >
+                            Check Out
+                        </Button>
+                    </div>
+            </div>
+        </>
+    );
+
+    if(!cart.line_items) return 'Loading...';
+
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title}
+                variant='h3'
+            >
+            Your Shopping Cart
+            </Typography>
+            { isEmpty ? <EmptyCart /> : <FilledCart /> }
+        </Container>
+    )
+}
+
+export default Cart;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from './navbar/navbar.component';
 export { default as Products } from './products/products.component';
+export { default as Cart } from './cart/cart.component';
