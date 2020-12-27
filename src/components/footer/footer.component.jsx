c79334a6d6cbdb8f454d93c2e8cc8ea7779c9783
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-12-27 01:59:22 -0500

    Added styling and functionality to social icons

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
 import useStyles from './footer.styles';
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
     const classes = useStyles();
     return (
         <AppBar position='static' className={classes.appbar}>
             <Container maxWidth='md'>
-                <Toolbar className={classes.toolbar}>
-                    <Typography variant='body1' color='inherit'>
-                        &copy; 2020 Vmaxtacular
-                    </Typography>  
-                </Toolbar>
+                    <Typography className={classes.icons} variant='body1'>
+                        <SocialIcon className={classes.icon} network='facebook' bgColor='white' url='https://www.facebook.com/Teton-Valley-Meats-116548596927746' /> 
+                        <SocialIcon className={classes.icon} network='instagram' bgColor='white' url='https://www.instagram.com/tetonvalleymeats/' />
+                    </Typography>    
             </Container>
         </AppBar>
     )
--- src/components/footer/footer.styles.js
+++ src/components/footer/footer.styles.js
@@ -4,5 +4,14 @@ export default makeStyles((theme) => ({
     appbar: {
         marginTop: '20px', 
         backgroundColor: '#7f0d0d',
+        padding: '1.5rem',
+    },
+    icons: {
+        textAlign: 'center',
+    },
+    icon: {
+        margin: 'auto 2rem',
+        height: '50px',
+        width: '50px',
     },
 }));
