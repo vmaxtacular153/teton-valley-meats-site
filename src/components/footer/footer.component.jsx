4dc89c50697fed09d1b63111e778915b89c736a2
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-12-27 00:29:06 -0500

    Added social icons and styling to footer component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import { Facebook, Instagram } from '@material-ui/icons';
+import useStyles from './footer.styles';
 
 const Footer = () => {
+    const classes = useStyles();
     return (
-        <AppBar position='static' color='primary' style={{marginTop: '20px'}}>
+        <AppBar position='static' className={classes.appbar}>
             <Container maxWidth='md'>
-                <Toolbar>
+                <Toolbar className={classes.toolbar}>
                     <Typography variant='body1' color='inherit'>
                         &copy; 2020 Vmaxtacular
                     </Typography>
+                        <Facebook fontSize='large' />
+                        <Instagram fontSize='large' />
                 </Toolbar>
             </Container>
         </AppBar>
---
+++ src/components/footer/footer.styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+    appbar: {
+        marginTop: '20px', 
+        backgroundColor: '#7f0d0d',
+    },
+    toolbar: {
+        display: 'inline-flex',
+        justifyItems: 'space-between',
+    },
+}));
